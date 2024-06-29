ee76cdfe28ac73d26886e6bdb07a54cd37951add
linahabita <habita.lina@univ-ouargla.dz>
2024-06-29 13:39:12 -0700

    lin2

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+#!/Users/mistarkelly/vagrant_project/My-Projects/ALX-ONLY/AirBnB_clone_v2/.venv/bin/python3
+This module instantiates an object of class DBStorage
+if the environment variable HBNB_TYPE_STORAGE is equal to db,
+otherwise it instantiates an object of class FileStorage
+"""
 from os import getenv
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
-
-if HBNB_TYPE_STORAGE == 'db':
-    from models.engine.db_storage import DBStorage
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
-    storage.relaod()
-
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.relaod()
+storage.reload()
--- models/place.py
+++ models/place.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer,Float,Table
 from sqlalchemy.orm import relationship
-from models import storage
-from models.review import Review
+from os import getenv
+
+place_amenity = Table(
+    'place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True)
+)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = "places"
-
+    __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
@@ -20,14 +25,19 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenity_ids = []
 
-    @property
-    def reviews(self):
-        """Returns a list of Review instances with place_id equals to
-        current Place.id"""
-        review_list = []
-        for review in storage.all(Review).values():
-            if review.place_id == self.id:
-                review_list.append(review)
-        return review_list
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', cascade='all, delete, delete-orphan', backref='place')
+        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False, back_populates='place_amenities')
+
+    else:
+        @property
+        def amenities(self):
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, amenity):
+            from models.amenity import Amenity
+            if isinstance(amenity, Amenity) and amenity.id not in self.amenity_ids:
+                self.amenity_ids.append(amenity.id)
