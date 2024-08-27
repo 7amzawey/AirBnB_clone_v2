92a045245e84726308d42fb5cbd31796eb8bbf0b
7amzawey <mohamedhamza123467890@gmail.com>
2024-08-27 18:29:03 +0200

    0x1A-application_sever task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
+@app.route('/airbnb-onepage', strict_slashes=False)
+def hello_again():
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
